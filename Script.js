9a8f30e65c931b808da3e7a69480ce95ceb133d3
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-08 22:59:58 +0300

    màj gestion formes

--- Script.js
+++ Script.js
@@ -1,11 +1,18 @@
-﻿document.getElementById("base").onkeyup = function(){
+document.getElementById("base").onkeyup = function(){
 	var PokémonText = this.value;
 	var PokémonArray = PokémonText.split(" ");
 	var PokémonList = Object.keys(Pokémon);
 	var finalArray = [];
 	for(i = 0; i < PokémonList.length; i++){
 		if (PokémonArray.indexOf(PokémonList[i]) != -1 && finalArray.indexOf(PokémonList[i]) == -1){ // Pour éviter les doublons
-			finalArray.push(PokémonList[i]);
+			if(PokémonList[i].indexOf("-") != -1){
+				var beforeDash = PokémonList[i].substring(0,PokémonList[i].indexOf("-"));
+				var afterDash = PokémonList[i].replace(beforeDash+"-","");
+				finalArray.push(beforeDash + " Forme " + afterDash);
+			}
+			else {
+				finalArray.push(PokémonList[i]);
+			}
 		}
 	}
 	document.getElementById("result").value = finalArray.join("/");
