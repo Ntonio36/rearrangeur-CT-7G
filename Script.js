f9f5cdc0ffdd78983678583004f413945b5a038a
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-08 22:44:36 +0300

    Application

---
+++ Script.js
@@ -0,0 +1,12 @@
+﻿document.getElementById("base").onkeyup = function(){
+	var PokémonText = this.value;
+	var PokémonArray = PokémonText.split(" ");
+	var PokémonList = Object.keys(Pokémon);
+	var finalArray = [];
+	for(i = 0; i < PokémonList.length; i++){
+		if (PokémonArray.indexOf(PokémonList[i]) != -1 && finalArray.indexOf(PokémonList[i]) == -1){ // Pour éviter les doublons
+			finalArray.push(PokémonList[i]);
+		}
+	}
+	document.getElementById("result").value = finalArray.join("/");
+};
