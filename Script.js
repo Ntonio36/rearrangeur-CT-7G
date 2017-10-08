7f6757a97bed336d437c5521ff50066cd74e00bc
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-09 01:28:21 +0300

    Formes Alola

--- Script.js
+++ Script.js
@@ -1,11 +1,18 @@
 document.getElementById("base").onkeyup = function(){
 	var PokémonText = this.value;
-	PokémonText = PokémonText.replace(/\{\{Poké\|\d{1,3}\|/g,"");
+	var alolanFlag = false;
+	PokémonText = PokémonText.replace(/\{\{Poké\|\d{1,3}/g,"");
 	PokémonText = PokémonText.replace(/\}\}/g,"");
 	var PokémonArray = PokémonText.split("\n");
 	var finalArray = [];		
 	for (i = 0; i < PokémonArray.length; i++){
-		finalArray.push(PokémonArray[i]);
+		if(PokémonArray[i].indexOf("d'Alola") != -1){
+			alolanFlag = true;
+			PokémonArray[i] = PokémonArray[i].replace(" d'Alola","");
+		}
+		PokémonArray[i] = PokémonArray[i].replace("|","");
+		var currentText = PokémonArray[i];
+		finalArray.push(PokémonArray[i]+(alolanFlag?" forme Alola":""));
 	}
 	document.getElementById("result").value = finalArray.join("/");
 };
