4cae6171138ddee95290109f8f1e6d6c9d38335e
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-09 00:42:06 +0300

    Refonte
    Merci Vaporeon, merci Évolimiqui, et GG Tonio

--- Script.js
+++ Script.js
@@ -1,28 +1,11 @@
 document.getElementById("base").onkeyup = function(){
 	var PokémonText = this.value;
-	var PokémonArray = PokémonText.split(" ");
-	var PokémonList = Object.keys(Pokémon);
-	var finalArray = [];
-	for (i = 0; i < PokémonList.length; i++){
-		if (PokémonArray.indexOf(PokémonList[i]) != -1 && finalArray.indexOf(PokémonList[i]) == -1){ // Pour éviter les doublons
-			if (PokémonList[i].indexOf("-") != -1){
-				var beforeDash = PokémonList[i].substring(0,PokémonList[i].indexOf("-"));
-				var afterDash = PokémonList[i].replace(beforeDash+"-","");
-				var pseudoFinalText = beforeDash+"-"+afterDash;
-				if (pseudoFinalText.indexOf("Méga-") != -1 && pseudoFinalText.indexOf("Primo-") != -1){
-					continue;
-				}
-				else if (pseudoFinalText == "Ho-Oh"){
-					finalArray.push("Ho-Oh");
-				}
-				else {
-					finalArray.push(beforeDash + " Forme " + afterDash);
-				}
-			}
-			else {
-				finalArray.push(PokémonList[i]);
-			}
-		}
+	PokémonText = PokémonText.replace(/\{\{Poké\|\d{1,3}\|/g,"");
+	PokémonText = PokémonText.replace(/\}\}/g,"");
+	var PokémonArray = PokémonText.split("\n");
+	var finalArray = [];		
+	for (i = 0; i < PokémonArray.length; i++){
+		finalArray.push(PokémonArray[i]);
 	}
 	document.getElementById("result").value = finalArray.join("/");
 };
