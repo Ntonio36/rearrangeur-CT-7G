510bc135a8aedaee64e4546a344df6bb6dab5726
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-10-08 23:41:02 +0300

    fix Ho-Oh + formes méga-/primo-

--- Script.js
+++ Script.js
@@ -3,12 +3,21 @@ document.getElementById("base").onkeyup = function(){
 	var PokémonArray = PokémonText.split(" ");
 	var PokémonList = Object.keys(Pokémon);
 	var finalArray = [];
-	for(i = 0; i < PokémonList.length; i++){
+	for (i = 0; i < PokémonList.length; i++){
 		if (PokémonArray.indexOf(PokémonList[i]) != -1 && finalArray.indexOf(PokémonList[i]) == -1){ // Pour éviter les doublons
-			if(PokémonList[i].indexOf("-") != -1){
+			if (PokémonList[i].indexOf("-") != -1){
 				var beforeDash = PokémonList[i].substring(0,PokémonList[i].indexOf("-"));
 				var afterDash = PokémonList[i].replace(beforeDash+"-","");
-				finalArray.push(beforeDash + " Forme " + afterDash);
+				var pseudoFinalText = beforeDash+"-"+afterDash;
+				if (pseudoFinalText.indexOf("Méga-") != -1 && pseudoFinalText.indexOf("Primo-") != -1){
+					continue;
+				}
+				else if (pseudoFinalText == "Ho-Oh"){
+					finalArray.push("Ho-Oh");
+				}
+				else {
+					finalArray.push(beforeDash + " Forme " + afterDash);
+				}
 			}
 			else {
 				finalArray.push(PokémonList[i]);
