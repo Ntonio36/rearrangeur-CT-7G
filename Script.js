f63397b44602b2b8199e5027da57f8b543a5dc67
Ntonio36 <32229257+Ntonio36@users.noreply.github.com>
2017-11-22 21:29:11 +0200

    Màj USUL

--- Script.js
+++ Script.js
@@ -18,7 +18,14 @@ function getWikicode(){
 				current = current.remove(/\{\{Poké\|.*?\|/);
 				current = current.toString().remove(/\}\}/).toString();
 				if(isForm){
-					current = current.replace("d'Alola","forme Alola").replace("Libegon","Libégon").replace("Monaflemit","Monaflèmit").replace("Morpheo","Morphéo").replace("M.Mime","M. Mime");
+					var toReplace = ["Libegon","Monaflemit","Morpheo","M.Mime"];
+					var toInsert = ["Libégon","Monaflèmit","Morphéo","M. Mime"];
+					
+					for(i = 0; i < toReplace.length; i++){
+						if(current.indexOf(toReplace[i]) != -1){
+							current = current.replace(toReplace[i],toInsert[i]);
+						}
+					}
 				}
 				finalArray.push(current);
 			}
@@ -29,7 +36,15 @@ function getWikicode(){
 		while(learnersList.indexOf("TM/HM") != -1){
 			learnersList.splice(0,1);
 		}
-		var PokémonList = Object.keys(EnglishPokémon);
+		switch(true){
+			case document.getElementById("SL").checked :
+				var PokémonList = Object.keys(EnglishPokémon);
+			break;
+			case document.getElementById("USUL").checked :
+				var PokémonList = Object.keys(EnglishPokémonUSUM);
+			break;
+			default : "";
+		}
 		for(x = 0; x < learnersList.length; x++){
 			var currentRow = learnersList[x];
 			for(i = 0; i < PokémonList.length; i++){
@@ -58,18 +73,33 @@ function getWikicode(){
 
 String.prototype.remove = function(part){
 	return this.replace(part,"");
-}
+};
 
-var inputs = document.querySelectorAll("input[type='radio']");
+var inputs = document.querySelectorAll("input[name='usage']");
 for(i = 0; i < inputs.length; i++){
 	inputs[i].addEventListener("click",function(){
 		var id = this.id;
 		switch(id){
-			case "translate" : document.getElementById("changeText").innerHTML = "Copiez-collez depuis le Pokédex Pokémon Showdown. Ne prenez que l'encadré TM/HM.";
+			case "translate" : 
+				document.getElementById("changeText").innerHTML = "Copiez-collez depuis le Pokédex Pokémon Showdown. Ne prenez que l'encadré TM/HM.";
+				document.getElementById("Gen7Games").style.display = "inline";
 			break;
-			case "convert" : document.getElementById("changeText").innerHTML = "Copiez-collez depuis l'éditeur de wikicode de Poképédia.";
+			case "convert" : 
+				document.getElementById("changeText").innerHTML = "Copiez-collez depuis l'éditeur de wikicode de Poképédia.";
+				document.getElementById("Gen7Games").hide();
 			break;
-			default : document.getElementById("changeText").innerHTML = "Admirez ce texte. Ou pas.";
+			default : 
+				document.getElementById("changeText").innerHTML = "Admirez ce texte. Ou pas.";
+				document.getElementById("Gen7Games").hide();
 		}
 	});
 }
+
+Object.prototype.hide = function(){
+	this.style.display = "none";
+	var nodes = this.childNodes;
+	for(i = 0; i < nodes.length; i++){
+		nodes[i].checked = false;
+		nodes[i].value = "";
+	}
+};
